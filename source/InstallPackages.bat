cf17bdba62065352c0f9443e54a7c3c14d1828a0
kazimanzurrashid <kazimanzurrashid@gmail.com>
2011-10-16 01:11:54 +0600

    Fixed the cassing in namespace.

--- source/InstallPackages.bat
+++ source/InstallPackages.bat
@@ -1,4 +1,4 @@
-tools\nuget\NuGet.exe i LetsCodeWIthMe.DataAccess.Community\packages.config -o packages
+tools\nuget\NuGet.exe i LetsCodeWithMe.DataAccess.Community\packages.config -o packages
 tools\nuget\NuGet.exe i LetsCodeWithMe.DataAccess.Community.Integration.Specs\packages.config -o packages
 tools\nuget\NuGet.exe i LetsCodeWithMe.Web\packages.config -o packages
 tools\nuget\NuGet.exe i LetsCodeWithMe.Web.Unit.Specs\packages.config -o packages
